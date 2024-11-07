53876ce2bc91dbd694e8fceb7e3851dad600551f
Mora <gustavo.mora4076@alumnos.udg.mx>
2024-11-06 23:29:11 -0600

    feature: create profiles

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->string('code', 9)->nullable();
             $table->string('rfc', 13)->nullable();
---
+++ database/migrations/2024_11_07_050149_create_perfiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('perfiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('degree_id');
+            $table->date('end_studies_date')->nullable();
+            $table->string('actual_job')->nullable();
+            $table->string('actual_company')->nullable();
+            $table->string('time_current_company')->nullable();
+            $table->string('first_job')->nullable();
+            $table->text('description_first_job');
+            $table->json('hardskills')->nullable();
+            $table->json('technologies')->nullable();
+            $table->json('certifications')->nullable();
+            $table->json('softskills')->nullable();
+            $table->json('proyects_practicies')->nullable();
+            $table->json('extras_cources')->nullable();
+            $table->string('council');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('perfiles');
+    }
+};
