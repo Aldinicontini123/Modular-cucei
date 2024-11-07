41e8ab0ef892e500448ff29d40ef544c095b91bd
Ruben1419 <113152768+Ruben1419@users.noreply.github.com>
2024-11-06 22:32:53 -0600

    SeederUsuario

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,11 +10,10 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        // Crea un usuario específico
         User::create([
-            'name' => 'Test',
-            'email' => 'Test@udg.com',
-            'password' => Hash::make('password'), // Recuerda encriptar la contraseña
+            'name' => 'test',
+            'email' => 'test@udg.com',
+            'password' => Hash::make('password'),
         ]);
     }
 }
