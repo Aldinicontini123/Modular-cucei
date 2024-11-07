76560bccb85af79fbbb85511d385c95a59ee711c
Mora <gustavo.mora4076@alumnos.udg.mx>
2024-11-07 00:16:16 -0600

    feature: changes foreing key in users an profiles

--- database/migrations/2024_11_07_050149_create_profiles_table.php
+++ database/migrations/2024_11_07_050149_create_profiles_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
             $table->foreignId('degree_id');
             $table->date('end_studies_date')->nullable();
             $table->string('actual_job')->nullable();
